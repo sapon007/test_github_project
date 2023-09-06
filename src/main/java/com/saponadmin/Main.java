99a786e694845a94ff91a2f126e27dec949341a4
Dmitry Saponov <saponadmin@gmail.com>
2023-09-06 22:49:19 +0300

    message is placed to variable

--- src/main/java/com/saponadmin/Main.java
+++ src/main/java/com/saponadmin/Main.java
@@ -2,7 +2,7 @@ package com.saponadmin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA guru!");
-        System.out.println("Cycle has been removed");
+        String msg = "Hello QA guru";
+        System.out.println(msg);
     }
 }
