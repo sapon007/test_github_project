486b74e4482d817dc1fc75f8c15e9e202a6224eb
Дмитрий Сапонов <46797382+sapon007@users.noreply.github.com>
2023-09-06 22:35:45 +0300

    Update Main.java

--- src/main/java/com/saponadmin/Main.java
+++ src/main/java/com/saponadmin/Main.java
@@ -2,7 +2,6 @@ package com.saponadmin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa guru!");
-        System.out.println("Cycle has been removed");
+        System.out.println("Hello QA GURU!");
     }
 }
